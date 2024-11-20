7d37c9b210c30956b4bb0bda76f04fee41bb56b9
Isaac González <isaac@homesusa.com>
2024-11-20 12:22:32 -0300

    Feature/application-layer - Added: UserService, PaymentService, UserContextProviderService and TokenProviderService Implementations

---
+++ Mobility.Payments.Application/Models/PaymentDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Mobility.Payments.Application.Models
+{
+    using Mobility.Payments.Crosscuting.Enums;
+    using System;
+
+    public class PaymentDto
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual decimal Amount { get; set; }
+
+        public virtual string SenderId { get; set; }
+
+        public virtual string ReceiverId { get; set; }
+
+        public virtual PaymentStatus PaymentStatus { get; set; }
+    }
+}
